a3393091b6548c71f23a91daed4e02af1bbf0510
lopintipaparao <Lopinti>
2023-02-05 17:26:48 +0530

    "I have updated"

--- Git_Eclipse/src/common_Methods/details1.java
+++ Git_Eclipse/src/common_Methods/details1.java
@@ -1,3 +1,18 @@
-name : papa rao
-village : rp
-state ; ap
+package common_Methods;
+
+import java.util.Scanner;
+
+public class details1 {
+
+	public static void main(String[] args) {
+		
+
+Scanner scan = new Scanner(System.in);
+
+String name = "Paparao", Village ="RP", State="AP";
+
+System.out.println(name +" "+ Village +" "+ State);
+scan.close();
+
+}
+}
